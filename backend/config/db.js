3991501f50deb986fb83ccc643aa0d487773bed1
Tamojit Mandal <tamojitm23@gmail.com>
2025-04-20 13:48:56 +0530

    Added mongo connection uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.set("strictQuery", true);
 
-async function connectToMongoDB(url) {
+async function connectToMongoDB() {
   try {
-    mongoose.connect(url);
+    mongoose.connect(process.env.MONGO_CONNECTION_URL);
   } catch (error) {
     console.error("DB connection Failed: ", error);
     process.exit(1);
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const connectDB = require("./config/db");
 const { requestLogger } = require("./middlewares/requestLogger")
 const userRouter = require("./routes/user-route");
+require("dotenv").config();
 
 const app = express();
-const PORT = 8000;
 
-connectDB("mongodb://127.0.0.1:27017/byte-tracker-bank_").then(() => console.log("MongoDB connected...\n")) 
+connectDB().then(() => console.log("MongoDB connected...\n")) 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +15,4 @@ app.use(requestLogger("./log.txt"));
 app.use("/api/", userRouter);
 
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+app.listen(process.env.PORT || 8000, () => console.log(`Server listening on ${process.env.PORT}`));
